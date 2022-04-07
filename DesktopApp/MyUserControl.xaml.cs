7babfe6898e5e632297e8f871ec78a4b90cdc4cd
sjf98 <nvenigma11@gmail.com>
2022-04-07 09:56:11 +0100

    first version
    work in progress

---
+++ DesktopApp/MyUserControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MyUserControl.xaml
+    /// </summary>
+    public partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+         
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //step 1 - get the text from text box, take the text from form of string to user textbox
+            string newLine = Environment.NewLine;
+            var text =  "Hey! Thank you for your interest in Hey! You can sign up to our platform here, www.ooohey.com" +newLine+ "Once you've signed up and connected your Instagram we'll get in touch when we've found a campaign that suits you!";
+
+
+
+            Clipboard.SetText(text);
+
+        }
+    }
+   
+}
